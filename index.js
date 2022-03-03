bdb35a8f0e8ba86365323ecdd9bbee601ee0a47f
bagusgandhi <bagusgandhi4@gmail.com>
2022-03-03 15:30:27 +0700

    fixed the port

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const http = require('http');
 const app = require('./app');
 const server = http.createServer(app);
 
-const { API_PORT } = process.env;
-const port = process.env.PORT || API_PORT;
 
-server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+
+
+server.listen(peocess.env.PORT || 3000 );
